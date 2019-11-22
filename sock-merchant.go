4c0ed9b88e70676e05745a271769956075e1ef0b
John McKee <jmckee3@mac.com>
2019-11-21 16:36:03 -0800

    working version

--- sock-merchant.go
+++ sock-merchant.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
+func sockMerchant(n int32, ar []int32) int32 {
+	sockMap := make(map[int32]int32)
+	var total int32
+
+	for i := int32(0); i < n; i++ {
+		sockMap[ar[i]]++
+	}
+
+	for key, value := range sockMap {
+		fmt.Println("key:", key, "value:", value)
+		total += value / 2
+	}
+
+	return total
+}
+
 func main() {
 
 }
--- spec-sock-merchant_test.go
+++ spec-sock-merchant_test.go
@@ -3,8 +3,14 @@ package main
 import "testing"
 
 func TestSockMerchant1(t *testing.T) {
-	// if cost != 7 {
-	// 	t.Errorf("got %d instead of 7", cost)
-	// }
+	ar := []int32{10, 20, 20, 10, 10, 30, 50, 10, 20}
+
+	socks := sockMerchant(int32(len(ar)), ar)
+
+	if socks != 3 {
+		t.Errorf("got %d instead of 3", socks)
+	}
 
 }
+
+// func sockMerchant(n int32, ar []int32) int32 {
